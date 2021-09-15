7c3bedfa686b98f3dc79a687ca70fd7f23cd07b0
Chiro <sachinsinghroxx@gmail.com>
2021-09-15 12:32:01 +0530

    added style js

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,27 @@
 import React from "react";
 import Head from "next/head";
 
+import useStyles from "../utils/styles";
+
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         {" "}
         <title>Mock App</title>
       </Head>
-      <AppBar postion="static">
+      <AppBar postion="static" className={classes.navebar}>
         <Toolbar>
           <Typography>Mock Website</Typography>
         </Toolbar>
       </AppBar>
 
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
 
-      <footer>
-          <Typography>
-              All rights reserved. Mock Website
-          </Typography>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Mock Website</Typography>
       </footer>
     </div>
   );
