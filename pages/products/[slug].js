23067c972a7bd30fda9614f3e319a4728579fc9a
Chiro <sachinsinghroxx@gmail.com>
2021-09-19 17:45:19 +0530

    cleaning for eslint

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,6 +1,4 @@
-import { useRouter } from "next/dist/client/router";
 import React from "react";
-import data from "../../utils/data";
 import Layout from "../../components/Layout";
 import NextLink from "next/link";
 import Image from "next/image";
